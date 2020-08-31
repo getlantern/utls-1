f6fd0e98d85cd3f99dd71e4f74718ca91883b112
Harry Harpham <harry@getlantern.org>
2020-08-31 08:17:21 -0600

    Fix channel ID extension ID

--- u_common.go
+++ u_common.go
@@ -20,7 +20,7 @@ const (
 	utlsExtensionExtendedMasterSecret uint16 = 23 // https://tools.ietf.org/html/rfc7627
 
 	// extensions with 'fake' prefix break connection, if server echoes them back
-	fakeExtensionChannelID uint16 = 30032 // not IANA assigned
+	fakeExtensionChannelID uint16 = 30031 // not IANA assigned
 
 	fakeCertCompressionAlgs uint16 = 0x001b
 	fakeRecordSizeLimit     uint16 = 0x001c
